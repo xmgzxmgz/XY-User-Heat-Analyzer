409032d181d391d0ae095377d61e36e823f8d00e
xmgz <xmgzdm@gmail.com>
2026-06-30 04:14:57 +0800

    Fix CORS, clean .gitignore, remove __pycache__

--- app/server.py
+++ app/server.py
@@ -30,7 +30,7 @@ app = FastAPI(title="XY User Heat Analyzer API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
